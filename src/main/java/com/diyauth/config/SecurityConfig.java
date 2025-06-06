932d36c63465ba961822fc57b733a7c5e97e69de
shivajshankar <shivajshankarblog@gmail.com>
2025-06-06 17:28:21 +0530

    cors has to be there

--- src/main/java/com/diyauth/config/SecurityConfig.java
+++ src/main/java/com/diyauth/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.disable())
+            .cors().and()
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
