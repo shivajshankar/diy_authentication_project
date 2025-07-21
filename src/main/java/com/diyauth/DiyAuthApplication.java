0300bb5ee063e6270684a86c53d1e97482ed5b9b
shivajshankar <shivajshankarblog@gmail.com>
2025-07-21 10:11:06 +0530

    adding a log to diyauthapp

--- src/main/java/com/diyauth/DiyAuthApplication.java
+++ src/main/java/com/diyauth/DiyAuthApplication.java
@@ -5,13 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableMongoAuditing
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class DiyAuthApplication {
+    private static final Logger logger = LoggerFactory.getLogger(DiyAuthApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DiyAuthApplication.class, args);
+        logger.info("DIY Auth Application started successfully");
     }
 }
